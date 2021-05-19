6cc3a863858a4d8da1106559091d21c5d52f269b
fabien CCDC <fcolard9@gmail.com>
2021-05-19 06:04:00 +0200

    1st

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { } from 'react-native';
-import { } from 'react-navigation';
-import Page_initale from './vues/page_initiale';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <Page_initale />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- README.md
+++ README.md
@@ -4,15 +4,24 @@
 Install nodejs and npm : https://nodejs.org/en/  
 Install yarn :
 ```bash
-$ npm install --global yarn
+npm install --global yarn
+```
+Install expo :
+```bash
+npm install -g expo-cli
 ```
 
 Clone the repository and install the dependencies
 ```bash
-$ git clone https://github.com/ProjectGiftApp/frontend
-$ cd frontend
-$ npm i
-$ yarn install
+git clone https://github.com/ProjectGiftApp/frontend
+cd frontend
+npm i
+yarn install
 ```
 
 And that's it!
+
+# How to start the application 
+```bash
+yarn start
+```
