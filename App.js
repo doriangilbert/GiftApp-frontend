799a4fd8f65d714b3b2280fd2d1ee4bf09f0249b
fabien CCDC <fcolard9@gmail.com>
2021-05-19 05:19:18 +0200

    + Readme

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { } from 'react-native';
+import { } from 'react-navigation';
 import Page_initale from './vues/page_initiale';
 
 export default function App() {
---
+++ README.mk
@@ -0,0 +1,18 @@
+# Front End of GiftApp Application
+
+# How to install
+Install nodejs and npm : https://nodejs.org/en/
+Install yarn : 
+```bash
+$ npm install --global yarn
+```
+
+Clone the repository and install the dependencies
+```bash
+$ git clone https://github.com/ProjectGiftApp/frontend
+$ cd frontend
+$ npm i
+$ yarn install
+```
+
+And that's it!
